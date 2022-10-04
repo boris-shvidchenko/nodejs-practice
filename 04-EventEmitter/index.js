4e176f156eeccf36dc0e6dd2ab5c9d24707fbf51
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-10-04 14:45:12 -0700

    Add files via upload

---
+++ 04-EventEmitter/index.js
@@ -0,0 +1,13 @@
+const logEvents = require('./logEvents');
+const EventEmmitter = require('events');
+class MyEmitter extends EventEmmitter {};
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    myEmitter.emit('log', 'Log event emitted.');
+}, 2000);
