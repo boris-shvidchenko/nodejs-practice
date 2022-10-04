3756c72f703c39117aa7c9040976695517397f42
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-10-04 14:46:02 -0700

    Add files via upload

---
+++ 05-WebServer/logEvents.js
@@ -0,0 +1,28 @@
+// event emitters
+// npm modules
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+// common core modules
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+async function logEvents(message, logName) {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+    console.log(logItem);
+    // creates a new log file if it does not exist already, otherwise append to existing file
+    try {
+        // if logs folder doesn't exist, create new directory ('logs)
+        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+    } catch (err) {
+        console.log(err);
+    }
+} 
+
+// export module 
+module.exports = logEvents;
